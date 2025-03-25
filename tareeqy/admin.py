1b93712e6d051943b965307696b8ba94f250bbd5
omaima shtiwe <omaimashtiwe@gmail.com>
2025-03-25 20:39:18 +0200

    v4

--- tareeqy/admin.py
+++ tareeqy/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Fence, FenceStatus
 
-# Register your models here.
+@admin.register(Fence)
+class FenceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude')
+    search_fields = ('name',)
+
+@admin.register(FenceStatus)
+class FenceStatusAdmin(admin.ModelAdmin):
+    list_display = ('fence', 'status', 'message_time')
+    list_filter = ('status',)
+    search_fields = ('fence__name',)
