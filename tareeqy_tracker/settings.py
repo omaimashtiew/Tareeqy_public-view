8297c8e7fbaa6bec3eb667bf4baefe3140d1211c
Sami Abdelrazeq <92661152+Samiabdelraezq@users.noreply.github.com>
2025-06-05 17:46:02 +0300

    redis

--- tareeqy_tracker/settings.py
+++ tareeqy_tracker/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 
 ]
 
@@ -129,9 +130,13 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': config("REDIS_URL")  # هذا هو المتغير اللي Railway ضايفه تلقائي
+        'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
     }
 }
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
